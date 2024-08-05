0d77ea1a9f6cb431efdbadeff4a5f9c18cb5859d
antoineedwards <edwardsantoine333@gmail.com>
2024-08-05 13:28:38 -0500

    2:28 mon

--- html5up-miniport/assets/js/contact.js
+++ html5up-miniport/assets/js/contact.js
@@ -3,15 +3,11 @@ const email = document.getElementById("email")
 const subject = document.getElementById("subject")
 const message = document.getElementById("message")
 
-function sendEmail() {
-    Email.send({
-        Host : "smtp.mailslurp.com",
-        Username : "<mailslurp username>",
-        Password : "<mailslurp password>",
-        To : 'recipient@example.com',
-        From : "sender@example.com",
-        Subject : "Test email",
-        Body : "<html><h2>Header</h2><strong>Bold text</strong><br/></br><em>Italic</em></html>"
-    }).then(message => alert(message)
-    );
-    }
+emailjs.send('service_id', 'template_id', {
+    to_email: 'edwardsantoine333@gmail.com',
+    from_name: email,
+    subject: 'Test email',
+    message_html: '<html><h2>Header</h2><strong>Bold text</strong><br><em>Italic</em></html>'
+}, 'user_id')
+.then(response => alert('Email sent successfully'))
+.catch(error => alert('Failed to send email'));
