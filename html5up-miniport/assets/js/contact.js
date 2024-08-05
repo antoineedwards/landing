6d2bb710f292b24eae1ebc4e46454306aab32119
antoineedwards <edwardsantoine333@gmail.com>
2024-08-05 13:48:37 -0500

    2:48 mon

--- html5up-miniport/assets/js/contact.js
+++ html5up-miniport/assets/js/contact.js
@@ -3,11 +3,14 @@ const email = document.getElementById("email")
 const subject = document.getElementById("subject")
 const message = document.getElementById("message")
 
-emailjs.send('service_id', 'template_id', {
-    to_email: 'edwardsantoine333@gmail.com',
-    from_name: email,
-    subject: 'Test email',
-    message_html: '<html><h2>Header</h2><strong>Bold text</strong><br><em>Italic</em></html>'
-}, 'user_id')
-.then(response => alert('Email sent successfully'))
-.catch(error => alert('Failed to send email'));
+function sendEmail() {
+    emailjs.send('service_ccazuv6', 'template_jgdp09t', {
+        to_email: 'edwardsantoine333@gmail.com',
+        from_name: email,
+        subject: 'Test email',
+        message_html: '<html><h2>Header</h2><strong>Bold text</strong><br><em>Italic</em></html>'
+    }, 'user_id')
+        .then(response => alert('Email sent successfully'))
+        .catch(error => alert('Failed to send email'));
+}
+document.getElementById('sendButton').addEventListener('click', sendEmail)
